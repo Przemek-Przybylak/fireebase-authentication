f0a7e7580573d279de7df250fc202def7ebe992e
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-06-26 21:47:37 +0200

    added routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import Reset from "./Reset/Reset";
+import Dashboard from "./Dashboard/Dashboard";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
-
 export default App;

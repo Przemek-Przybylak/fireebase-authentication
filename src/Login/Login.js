6f111e9a0cbadf71c1fa06b0ee71b677e9d80313
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-07 19:50:00 +0200

    logInWithEmailAndPassword on click added

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, signInWithGoogle } from "../firebase";
+import { auth, logInWithEmailAndPassword, signInWithGoogle } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./Login.css";
-import { selectAuth } from "../firebaseSlice";
-import { useSelector } from "react-redux";
 
 function Login() {
-  const auth = useSelector(selectAuth);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading, error] = useAuthState(auth);
@@ -38,7 +35,7 @@ function Login() {
         />
         <button
           className="login__btn"
-          onClick={() => signInWithEmailAndPassword(email, password)}
+          onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
         </button>

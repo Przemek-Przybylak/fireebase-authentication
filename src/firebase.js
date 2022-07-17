bd04ddf124af7701316638d2fe474f837e41ef6d
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-07-17 13:02:41 +0200

    added right firbase configuration in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -17,13 +17,12 @@ import {
   addDoc,
 } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyD6BmUpYsB5fSyK2vDdGLIOde_hS1noVNY",
+  authDomain: "fir-auth-first-time-5cc3a.firebaseapp.com",
+  projectId: "fir-auth-first-time-5cc3a",
+  storageBucket: "fir-auth-first-time-5cc3a.appspot.com",
+  messagingSenderId: "455698106220",
+  appId: "1:455698106220:web:b38f387829621596155549",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
